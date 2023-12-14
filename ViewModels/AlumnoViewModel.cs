3ae89096c8cd975e064dc5da70fa0f9acd4eb3f1
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-12-14 20:13:06 -0300

    Modifications/Problems Layout

--- ViewModels/AlumnoViewModel.cs
+++ ViewModels/AlumnoViewModel.cs
@@ -1,10 +1,12 @@
 using Instituto1.Models;
 
+namespace Instituto1.ViewModels;
+
 public class AlumnoViewModel {
 
     public List<Alumno> Alumnos {get;set;} = new List<Alumno>();
 
-    public string NameFilter {get;set;}
+    public string? NameFilter {get;set;}
 
 
 }
--- obj/Debug/net8.0/Instituto1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Instituto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Instituto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d7a364e8abf3b9737f6361e1d071e1333769437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c44acef01f9b6d595079ab7089085c1d36ce251")]
 [assembly: System.Reflection.AssemblyProductAttribute("Instituto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Instituto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
