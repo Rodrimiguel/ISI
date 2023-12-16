09afaa8ba6245f3ba3398d3c4c57b099822f9897
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-12-16 01:39:34 -0300

    Migrations CursoAlumno Update

---
+++ Migrations/20231216043717_Initial Update.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Instituto1.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alumno",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Dni = table.Column<string>(type: "TEXT", nullable: false),
+                    CursoSeleccionado = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alumno", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Duration = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Credits = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CursoAlumno",
+                columns: table => new
+                {
+                    AlumnosId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CursosId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoAlumno", x => new { x.AlumnosId, x.CursosId });
+                    table.ForeignKey(
+                        name: "FK_CursoAlumno_Alumno_AlumnosId",
+                        column: x => x.AlumnosId,
+                        principalTable: "Alumno",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CursoAlumno_Curso_CursosId",
+                        column: x => x.CursosId,
+                        principalTable: "Curso",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CursoAlumno_CursosId",
+                table: "CursoAlumno",
+                column: "CursosId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CursoAlumno");
+
+            migrationBuilder.DropTable(
+                name: "Alumno");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+        }
+    }
+}
