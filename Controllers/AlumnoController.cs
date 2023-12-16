d36d9bd4dbe99322bc9b7f44ea164a5e575dc496
RoderickMMM <rodrigomfoggia84@gmail.com>
2023-12-16 18:35:51 -0300

    Modifications Views/Alumno and AlumnoControllers

--- Controllers/AlumnoController.cs
+++ Controllers/AlumnoController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Instituto1.Data;
 using Instituto1.Models;
+using Instituto1.ViewModels;
 
 namespace Instituto1.Controllers
 {
